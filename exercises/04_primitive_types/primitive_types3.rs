a765eba364a5cab63c98d2ebb7b974341345aebf
Kabir <vatshivam49888@gmail.com>
2025-10-30 14:42:06 +0530

    Complete exercises till lesson 6

--- exercises/04_primitive_types/primitive_types3.rs
+++ exercises/04_primitive_types/primitive_types3.rs
@@ -1,6 +1,12 @@
+use std::vec;
+
 fn main() {
     // TODO: Create an array called `a` with at least 100 elements in it.
-    // let a = ???
+    let mut a: Vec<u32> = vec![];
+
+    for i in 0..100 {
+        a.push(i);
+    }
 
     if a.len() >= 100 {
         println!("Wow, that's a big array!");
